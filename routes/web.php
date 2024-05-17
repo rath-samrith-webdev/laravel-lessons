e4e920630280ab654d3948ca0556040d776f71ef
Roth Samreth <140585607+Roth-Samreth@users.noreply.github.com>
2024-05-17 08:56:47 +0700

    Add register user function

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,4 +23,5 @@ Route::get('/', function () {
 Route::prefix('/auth')->group(function () {
     Route::post('/login',LoginController::class)->name('login');
     Route::post('/logout',LogoutController::class)->name('logout');
+    Route::post('/register',RegisterController::class)->name('register');
 });
