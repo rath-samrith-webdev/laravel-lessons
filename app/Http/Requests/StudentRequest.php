758f84495045fa724538cf1ac7a12886f6b40328
Your Name <you@example.com>
2024-05-28 13:17:59 +0700

    Integrated api with vue js

---
+++ app/Http/Requests/StudentRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StudentRequest extends DefaultRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name' =>'required',
+            'age' =>'required|integer',
+            'phone' =>'required |unique:students,phone,NULL,id,deleted_at,NULL',
+        ];
+        return $rules;
+    }
+}
