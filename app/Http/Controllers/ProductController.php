edb35ef034e2c50b8d864e165c936ac22d5d25ef
Your Name <you@example.com>
2024-05-30 13:06:19 +0700

    Testing upload img to api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        if (count($products) > 0) {
+            return response()->json(['success' => true, 'message' => 'Products found', 'products' => ProductResource::collection($products)]);
+        }
+        return response()->json(['success' => false, 'message' => 'No Products found']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $data = $request->validated();
+        $image = $request->image;
+        $ext = $image->getClientOriginalExtension();
+        $imageName = time() . '.' . $ext;
+        $image->move(public_path('/') . 'upload/', $imageName);
+        return $imageName;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
