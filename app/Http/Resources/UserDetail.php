9810e07d2552c931a38331df464d0aa4ca3c38d7
Your Name <you@example.com>
2024-06-02 18:34:57 +0700

    V1 fb-api

--- app/Http/Resources/UserDetail.php
+++ app/Http/Resources/UserDetail.php
@@ -14,6 +14,10 @@ class UserDetail extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id"=>$this->id,
+            "username"=>$this->name,
+            'email'=>$this->email
+        ];
     }
 }
